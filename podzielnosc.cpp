7ac44d52f8e98868eebee644f1502c0465c5299b
TomJ2 <tomasz.janczak03@gmail.com>
2024-11-18 07:29:10 +0100

    Added check if numbers are positive

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
+
+int nwd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
+    
+    if (a <= 0 || b <= 0) {
+        cout << "Liczby muszą być dodatnie!" << endl;
+        return 1;
+    }
+
     cout << "Wczytane liczby: " << a << " " << b << endl;
+    cout << "NWD: " << nwd(a, b) << endl;
     return 0;
 }
+
 
